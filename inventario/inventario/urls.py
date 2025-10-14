1fe3d0c953134c978a8371a11c7f019c5526d34a
NV <noelia.vivas@itscordoba.edu.ar>
2025-10-13 23:28:18 -0300

    Configura index como página principal y agrega index.html

--- inventario/inventario/urls.py
+++ inventario/inventario/urls.py
@@ -6,18 +6,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static      
 from django.contrib.auth import views as auth_views
-from productos.views import ProductoListView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ProductoListView.as_view(), name='home'),
+    
+   
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('productos/', include('productos.urls')),
     path('clientes/', include('clientes.urls')),
     path('ventas/', include('ventas.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
